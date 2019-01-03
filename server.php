10a40ac20ac2f34d7bdf5c99f435af771cde630b
link834 <link834@gmail.com>
2019-01-03 12:49:23 -0700

    Made some server changes and added the test html file

--- server.php
+++ server.php
@@ -6,6 +6,13 @@
 
 <h1>Test</h1>
 
+<form action="server.php">
+    <input type="text" id="ip" name="ip" />
+    <input type="text" id="port" name="port" />
+    <input type="submit" name="start" value="Start Server" />
+    <input type="submit" name="stop" value="Stop Server" />
+</form>
+
 <?php
     /*if ($_POST['start']) {
         echo exec("python3 ./rage-server.py $_POST['ip'] $_POST['port'] &");
