02025f85f5a2208da94feb86e152af7d0fcf0ae3
link834 <link834@gmail.com>
2019-01-02 17:45:01 -0700

    server.php - only working with the stop action at the moment

--- server.php
+++ server.php
@@ -1,9 +1,5 @@
 <?php
     if ($_POST['start']) {
-        echo exec('python3 /home/pi/rage-server/rage-server.py &');
-    }
-
-    if ($_POST['stop']) {
-        echo exec('touch /home/pi/rage-server/stop-server');
+        echo exec("python3 ./rage-server.py $_POST['ip'] $POST['port'] &");
     }
 ?>
