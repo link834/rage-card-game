c17f6616bf5ca24b3f8f7f387278dea3cfad6789
link834 <link834@gmail.com>
2019-01-04 16:47:50 -0700

    added stop server and switched to system()

--- server.php
+++ server.php
@@ -7,8 +7,8 @@
 <h1>Test</h1>
 
 <form action="server.php" method="post">
-    <input type="text" id="ip" name="ip" />
-    <input type="text" id="port" name="port" />
+    <input type="text" id="ip" name="ip" required />
+    <input type="text" id="port" name="port" required />
     <input type="submit" name="start" value="Start Server" />
     <input type="submit" name="stop" value="Stop Server" />
 </form>
@@ -16,11 +16,20 @@
 <?php
     function startServer($ip, $port) {
         $nunya = $_SERVER['NUNYA'];
-        echo passthru("sudo python3 ./rage-server.py {$ip} {$port} {$nunya} > /dev/null 2>&1 &");
+        echo system(
+            "sudo python3 ./rage-server.py {$ip} {$port} {$nunya} > /dev/null 2>&1 & echo $! > ./python.pid"
+        );
+    }
+
+    function stopServer() {
+        echo system(
+            'sudo kill $(cat ./python.pid)'
+        );
     }
 
     if ($_POST['start']) {startServer($_POST['ip'], $_POST['port']);}
-    if ($_GET['start']) {startServer($_GET['ip'], $_GET['port']);}
+    if ($_POST['stop']) {stopServer();}
+    //if ($_GET['start']) {startServer($_GET['ip'], $_GET['port']);}
 ?>
 
 </body>
