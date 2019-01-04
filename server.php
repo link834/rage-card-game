26f8f09fe92868be4f240281043b05db855cb739
link834 <link834@gmail.com>
2019-01-04 16:01:05 -0700

    more debugging

--- server.php
+++ server.php
@@ -6,7 +6,7 @@
 
 <h1>Test</h1>
 
-<form action="server.php">
+<form action="server.php" method="post">
     <input type="text" id="ip" name="ip" />
     <input type="text" id="port" name="port" />
     <input type="submit" name="start" value="Start Server" />
@@ -17,8 +17,7 @@
     function startServer($ip, $port) {
         $nunya = $_SERVER['NUNYA'];
         echo shell_exec("sudo python3 ./rage-server.py {$ip} {$port} {$nunya} &");
-        echo "<h1>sudo python3 ./rage-server.py {$ip} {$port} {$nunya} &</h1>";
-        echo '<hr>$nunya = ' . $nunya . "<hr>";
+        echo "<hr>sudo python3 ./rage-server.py {$ip} {$port} {$nunya} &<hr>";
     }
 
     if ($_POST['start']) {startServer($_POST['ip'], $_POST['port']);}
